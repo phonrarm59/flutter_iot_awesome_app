eb1f1ab08e7b4eea5c7ef3f11aa2dff667708107
phonrarm59 <phonrarm59@gmail.com>
2026-01-30 21:02:58 +0700

    Commit 02 แก้ไขHOME ให้เปิดไปยังหน้าจออื่นได้และแก้ไขlogin ui แต่ยังไม่เสร็จ

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: sort_child_properties_last
 
 import 'package:flutter/material.dart';
+import 'package:flutter_iot_awesome_app/views/login_ui.dart';
+import 'package:flutter_iot_awesome_app/views/signup_ui.dart';
 
 class HomeUi extends StatelessWidget {
   const HomeUi({super.key});
@@ -9,86 +11,97 @@ class HomeUi extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 120.0,
-            ),
-            Image.asset(
-              'assets/images/logo.png',
-              width: 320.0,
-              height: 320.0,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 180.0,
-            ),
-            Text(
-              'Bulid Iot-SAU App',
-              style: TextStyle(
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 120.0,
               ),
-            ),
-            Text(
-              'มหาวิทยาลัยเอเชียอาคเนย์',
-              style: TextStyle(
-                  fontSize: 20.0,
+              Image.asset(
+                'assets/images/logo.png',
+                width: 320.0,
+                height: 320.0,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 180.0,
+              ),
+              Text(
+                'Bulid Iot-SAU App',
+                style: TextStyle(
+                  fontSize: 28.0,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            ),
-            Text(
-              'Createc by Lucky',
-              style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      color: Colors.black
-                    ),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    fixedSize: Size(150, 55),
-                  ),
+                  color: Colors.black,
                 ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'SIGN UP',
-                    style: TextStyle(
-                      color: Colors.white
+              ),
+              Text(
+                'มหาวิทยาลัยเอเชียอาคเนย์',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              Text(
+                'Createc by Lucky',
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginUi(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      fixedSize: Size(150, 55),
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    fixedSize: Size(150, 55),
-                    backgroundColor: Colors.black
+                  SizedBox(
+                    width: 20.0,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SingnupUi(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'SIGN UP',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        fixedSize: Size(150, 55),
+                        backgroundColor: Colors.black),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
